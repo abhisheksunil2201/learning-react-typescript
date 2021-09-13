5fa2b213cbb59880834e5911663b7f8749aff9df
Abhishek <abhisheksunil92@gmail.com>
2021-09-13 13:52:14 +0530

    lesson-6 useState Hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,9 @@
 import './App.css';
-import { Container } from './components/Container';
-
+import { LoggedIn } from './components/state/LoggedIn';
 function App() {
   return (
     <div className="App">
-      <Container styles={{border: '1px solid black',padding:'1rem'}}/>
+      <LoggedIn />
     </div>
   );
 }
