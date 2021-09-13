ab96e588100fece77d260ce55b11dc688aeb98e9
Abhishek <abhisheksunil92@gmail.com>
2021-09-13 14:00:51 +0530

    lesson-7 useState Hook Future Values

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import './App.css';
-import { LoggedIn } from './components/state/LoggedIn';
+import { User } from './components/state/User';
+
 function App() {
   return (
     <div className="App">
-      <LoggedIn />
+      <User />
     </div>
   );
 }
