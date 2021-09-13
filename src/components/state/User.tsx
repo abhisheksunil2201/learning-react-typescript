509a76987ffd23242162bbdaa42f59fd777b25b3
Abhishek <abhisheksunil92@gmail.com>
2021-09-13 14:04:10 +0530

    lesson-8 useState Hook Type assertion

--- src/components/state/User.tsx
+++ src/components/state/User.tsx
@@ -6,22 +6,19 @@ type AuthUser = {
 }
 
 export const User = () => {
-    const[user,setUser] = useState<AuthUser | null>(null)
+    const[user,setUser] = useState<AuthUser>({} as AuthUser)
     const handleLogin = () =>{
         setUser({
             name: "Abhishek",
             email: "abhi@gmail.com"
         })
     }
-    const handleLogout = () =>{
-        setUser(null)
-    }
     return (
         <div>
             <button onClick={handleLogin}>Login</button>
-            <button onClick={handleLogout}>Logout</button>
-            <div>User name is {user?.name}</div>
-            <div>User email is {user?.email}</div>
+            {/* Without optional chaining, we can access object using type assertion */}
+            <div>User name is {user.name}</div>
+            <div>User email is {user.email}</div>
         </div>
     )
 }
